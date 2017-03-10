4c702b6bad74298bbef2a95b580d9d44d764cd4c
Adam Hegedus <Adam_Hegedus@epam.com>
2017-03-10 17:26:59 +0100

    Simple build for gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var ts = require('gulp-typescript');
+var rename = require('gulp-rename');  
+var uglify = require('gulp-uglify'); 
+
+var project = ts.createProject('tsconfig.json');
+
+gulp.task('build:sass', function () {
+    return gulp.src('app/scss/index.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build:typescript', function () {
+    return gulp.src('app/**/*.ts')
+        .pipe(project())
+        .js.pipe(gulp.dest(''));
+});
+
+gulp.task('minify:js',  function () {
+    return gulp.src('dist/script/app.js')
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/script'));
+});
+
+gulp.task('build', ['build:sass', 'build:typescript', 'minify:js']);
